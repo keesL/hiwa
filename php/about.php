1b2ec0556bd731419589ff736dd9825b693a032f
Kees Leune <kees@compsci.adelphi.edu>
2017-10-24 19:27:04 -0400

    added about page with link to repo

---
+++ php/about.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>About HIWA</title>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+<?php require 'header.php'; ?>
+
+<p>HIWA is the <em>Horribly Insecure Web Application</em>.</p>
+
+<p>It is purposefully packed with all kinds of security vulnerabilities. I use
+the application to teach various web application security problems to
+undergraduate comptuer science students.</p>
+
+<div style="border:1px solid black; padding: 5px; text-align:center">
+<p style="font-size:125%; font-weight:bold;">DO NOT RUN THIS APPLICATION ON THE INTERNET<p>without controlling access to it!</p></div>
+
+<p>In other words: This product is guaranteed to lead to system compromise if youy put it on the Internet.</p>
+
+<p>The project is available via <a href="https://github.com/KeesL/hiwa">GitHub's HIWA Repo</a>.</p>
+
+</body>
+</html>
+
