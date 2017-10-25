8164c6d9953b0bd98a1bca8b7b5d5ea5d6fc299d
Kees Leune <kees@compsci.adelphi.edu>
2017-10-24 20:11:22 -0400

    moved logo img into uploads dir so it can be overwritten

--- php/header.php
+++ php/header.php
@@ -2,7 +2,7 @@
 
 <div class="header">
 <div class="logo"><a href="menu.php"><img src="<?php 
-	echo $CONFIG['logo'];?>" alt="[logo]"/></a><br/>
+	echo $CONFIG['images'].'/'.$CONFIG['logo'];?>" alt="[logo]"/></a><br/>
 <a href="https://github.com/keesl/hiwa" target="_blank">About</a>
 </div>
 <div class="branding"><?php echo $CONFIG['brandname'];?></div>
